928b173dc2b777484b89e12c8d6d39090692255e
Marcos Galvão <marcos_rmgalvao@hotmail.com>
2021-05-18 23:59:48 -0300

    Adicionando a ferramenta de seleção de período

--- src/analise.py
+++ src/analise.py
@@ -7,7 +7,8 @@ class SerieTemporal:
     '''
     --> Apresenta a série temporal dos investimentos analisados
     '''
-    def __init__(self, investimento=None, inpc=None, x_investimento=None, y_investimento=None, x_inpc=None, y_inpc=None):
+    def __init__(self, investimento=None, inpc=None, x_investimento=None, y_investimento=None, x_inpc=None, y_inpc=None, 
+                periodo=None):
         '''
         :param investimento: Série temporal do investimento
         '''
@@ -17,7 +18,8 @@ class SerieTemporal:
         self._y_investimento = y_investimento
         self._x_inpc = x_inpc
         self._y_inpc = y_inpc
-    
+        self._periodo = periodo
+        
     
     def serie_temporal(self, titulo=None, x_label=None, y_label=None, inpc='INPC'):
         '''
@@ -25,21 +27,27 @@ class SerieTemporal:
         :param x: Período analisado
         :param y: Pagamentos da série
         '''
+        # Variáveis de definição do intervalo de tempo 
+        periodo_investimento = self._investimento['data'].dt.year.between(self._periodo[0], self._periodo[1]) == True
+        periodo_inpc = self._inpc['data'].dt.year.between(self._periodo[0], self._periodo[1]) == True
+        
         fig = go.Figure()
-        fig.add_trace(go.Scatter(x=self._investimento[self._x_investimento], y=self._investimento[self._y_investimento],
-                      mode='lines',
-                      name='Poupança'))
-        fig.add_trace(go.Scatter(x=self._inpc[self._x_inpc], y=self._inpc[self._y_inpc],
-                      mode='lines',
-                      name=inpc,
-                      line_color='rgb(255,215,0)'))
+        fig.add_trace(go.Scatter(x=self._investimento[periodo_investimento][self._x_investimento], 
+                                y=self._investimento[periodo_investimento][self._y_investimento],
+                    mode='lines',
+                    name='Poupança'))
+        fig.add_trace(go.Scatter(x=self._inpc[periodo_inpc][self._x_inpc], 
+                                y=self._inpc[periodo_inpc][self._y_inpc],
+                    mode='lines',
+                    name=inpc,
+                    line_color='rgb(255,215,0)'))
         fig.add_trace(go.Indicator(
                     mode = "number+delta",
-                    value = np.sum(self._investimento[self._y_investimento]).round(2),
-                    delta = {"reference": np.sum(self._inpc[self._y_inpc]).round(2)},
-                    title = {"text": "Rendimento do Período"},
+                    value = np.sum(self._investimento[periodo_investimento][self._y_investimento]).round(2),
+                    delta = {"reference": np.sum(self._inpc[periodo_inpc][self._y_inpc]).round(2)},
+                    title = {"text": "Rendimento relativo a inflação"},
                     domain = {'y': [0, 1], 'x': [0.25, 0.75]}))
         fig.update_layout(title=titulo,
                         xaxis_title=x_label,
                         yaxis_title=y_label)
-        st.plotly_chart(fig)
+        st.plotly_chart(fig, hidth=1000)
--- src/app.py
+++ src/app.py
@@ -8,11 +8,15 @@ poupanca_df = investimentos.rendimentos_poupanca()
 inpc_df = investimentos.indice_inpc()
 
 def main():
+    # Seleção do período em anos
+    anos = poupanca_df['data'].dt.year.unique().tolist()
+    periodo = st.sidebar.slider('Selecione o período', min_value=min(anos), max_value=max(anos), value=(min(anos), max(anos)))
+
     st.title('Investimentos')
     analise_poupanca = SerieTemporal(investimento=poupanca_df, inpc=inpc_df, x_investimento='data', y_investimento='rentabilidade',
-                                    x_inpc='data', y_inpc='mes_%')
+                                    x_inpc='data', y_inpc='mes_%', periodo=periodo)
     analise_poupanca.serie_temporal(titulo='Rentabilidade Poupança', x_label='', y_label='%')
-
+    
 if __name__ == '__main__':
     main()
 
