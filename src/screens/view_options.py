da056e25d5c37c96137eb2e89eff3e488267f260
Marcos Galvão <marcos_rmgalvao@hotmail.com>
2023-01-23 10:52:00 -0300

    indentation fix

--- src/screens/view_options.py
+++ src/screens/view_options.py
@@ -20,29 +20,29 @@ def visualizations(analyzer: object, view: str, check: bool):
     Returns: Plotly Chart
                 Plotly chart of selected view
     '''
-        if check:
-            if view == 'Sazonalidade e Tendência':
-                st.subheader('Sazonalidade e Tendência')
-                analyzer.serie_decomposition()
-            elif view == 'Histograma':
-                st.subheader('Distribuição')
-                analyzer.histogram_view()
-            elif view == 'Boxplot':
-                st.subheader('Boxplot')
-                analyzer.boxplot_view()
-            elif view == 'Barplot':
-                # Aggregation options
-                function_dict = {'Soma': np.sum, 'Média': np.mean}
-                agg = st.selectbox('Período', ['Ano', 'Trimestre', 'Mês'])
-                function = st.selectbox('Função', function_dict.keys())
-                st.subheader('Barplot')
-                analyzer.barplot_view(aggregation=agg, function=function_dict[function])
-            elif view == 'Estatística Descritiva':
-                st.subheader('Estatística Descritiva')
-                analyzer.descriptive_statistics()
-            elif view == 'Correlação Linear':
-                st.subheader('Correlação Linear')
-                analyzer.correlation()
+    if check:
+        if view == 'Sazonalidade e Tendência':
+            st.subheader('Sazonalidade e Tendência')
+            analyzer.serie_decomposition()
+        elif view == 'Histograma':
+            st.subheader('Distribuição')
+            analyzer.histogram_view()
+        elif view == 'Boxplot':
+            st.subheader('Boxplot')
+            analyzer.boxplot_view()
+        elif view == 'Barplot':
+        # Aggregation options
+            function_dict = {'Soma': np.sum, 'Média': np.mean}
+            agg = st.selectbox('Período', ['Ano', 'Trimestre', 'Mês'])
+            function = st.selectbox('Função', function_dict.keys())
+            st.subheader('Barplot')
+            analyzer.barplot_view(aggregation=agg, function=function_dict[function])
+        elif view == 'Estatística Descritiva':
+            st.subheader('Estatística Descritiva')
+            analyzer.descriptive_statistics()
+        elif view == 'Correlação Linear':
+            st.subheader('Correlação Linear')
+            analyzer.correlation()
 
 def view_list():
     '''
